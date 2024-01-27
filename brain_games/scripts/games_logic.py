dc2e2ce08abd38354d5c028de3738a404e6a3ae1
Sergeeva Larisa <mindzu@mail.ru>
2024-01-27 12:23:01 +0500

    add game progression

--- brain_games/scripts/games_logic.py
+++ brain_games/scripts/games_logic.py
@@ -19,3 +19,15 @@ def gcd(number1, number2):
         else:
             number2 = result
     return result
+
+
+def is_correct(name, question, correct_answer):
+    print(f'Question: {question}')
+    answer = prompt.integer('Your answer: ')
+    if answer == correct_answer:
+        print('Correct!')
+        return True
+    else:
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+        print(f"Let's try again, {name}!")
+        return False
