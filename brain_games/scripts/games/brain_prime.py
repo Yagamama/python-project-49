2e033decf286da2fb8186cbc446ad9c5d7af9a5b
Sergeeva Larisa <mindzu@mail.ru>
2024-01-27 16:50:48 +0500

    add game brain-prime

---
+++ brain_games/scripts/games/brain_prime.py
@@ -0,0 +1,22 @@
+import random
+import brain_games.scripts.games_logic as gl
+
+
+def main():
+    name = gl.hello()
+    prime(name)
+
+
+def prime(name):
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    for i in range(gl.GAMES_COUNT):
+        number = random.randint(1,100)
+        correct_answer = gl.is_prime(number)
+        result = gl.is_correct_string(name, number, correct_answer)
+        if result == False:
+            return
+    print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
--- brain_games/scripts/games_logic.py
+++ brain_games/scripts/games_logic.py
@@ -1,4 +1,5 @@
 import prompt
+import math
 
 
 GAMES_COUNT = 3
@@ -11,16 +12,6 @@ def hello():
     return name
 
 
-def gcd(number1, number2):
-    while number1 != number2:
-        result = abs(number1 - number2)
-        if number1 > number2:
-            number1 = result
-        else:
-            number2 = result
-    return result
-
-
 def is_correct(name, question, correct_answer):
     print(f'Question: {question}')
     answer = prompt.integer('Your answer: ')
@@ -31,3 +22,32 @@ def is_correct(name, question, correct_answer):
         print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
         print(f"Let's try again, {name}!")
         return False
+    
+
+def is_correct_string(name, question, correct_answer):
+    print(f'Question: {question}')
+    answer = prompt.string('Your answer: ')
+    if answer == correct_answer:
+        print('Correct!')
+        return True
+    else:
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+        print(f"Let's try again, {name}!")
+        return False
+    
+
+def gcd(number1, number2):
+    while number1 != number2:
+        result = abs(number1 - number2)
+        if number1 > number2:
+            number1 = result
+        else:
+            number2 = result
+    return result
+
+
+def is_prime(number):
+    for i in range(2, round(math.sqrt(number))+1):
+        if number % i == 0:
+            return 'no'
+    return 'yes'
